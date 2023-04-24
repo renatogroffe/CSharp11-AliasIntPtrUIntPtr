2f0a820dafdb0bb3a77f67d08bfed436a255cfda
Renato Groffe <renato.groffe@yahoo.com.br>
2023-04-24 10:57:27 -0300

    C# 11 + Console Application + Aliases para IntPtr e UIntPtr (nint e nuint)
    C# 11 + Console Application + Aliases para IntPtr e UIntPtr (nint e nuint)

---
+++ ExemploAliasIntPtrUIntPtr/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.InteropServices;
+
+Console.WriteLine("***** Testes com C# 11 | Aliases para IntPtr e UIntPtr (nint e nuint) *****");
+Console.WriteLine($"Versao do .NET em uso: {RuntimeInformation
+    .FrameworkDescription} - Ambiente: {Environment.MachineName}- Kernel: {Environment
+    .OSVersion.VersionString}");
+
+Console.WriteLine();
+
+IntPtr intPtr01 = IntPtr.Zero;
+Console.WriteLine($"{nameof(intPtr01)} = {intPtr01} [{intPtr01.GetType().FullName}]");
+
+nint intPtr02 = nint.Zero;
+Console.WriteLine($"{nameof(intPtr02)} = {intPtr02} [{intPtr02.GetType().FullName}]");
+
+IntPtr intPtr03 = -10;
+Console.WriteLine($"{nameof(intPtr03)} = {intPtr03} [{intPtr03.GetType().FullName}]");
+
+nint intPtr04 = -15;
+Console.WriteLine($"{nameof(intPtr04)} = {intPtr04} [{intPtr04.GetType().FullName}]");
+
+Console.WriteLine();
+
+UIntPtr nuintPtr01 = UIntPtr.Zero;
+Console.WriteLine($"{nameof(nuintPtr01)} = {nuintPtr01} [{nuintPtr01.GetType().FullName}]");
+
+nuint nuintPtr02 = nuint.Zero;
+Console.WriteLine($"{nameof(nuintPtr02)} = {nuintPtr02} [{nuintPtr02.GetType().FullName}]");
+
+UIntPtr nuintPtr03 = 20;
+Console.WriteLine($"{nameof(nuintPtr03)} = {nuintPtr03} [{nuintPtr03.GetType().FullName}]");
+
+nuint nuintPtr04 = 30;
+Console.WriteLine($"{nameof(nuintPtr04)} = {nuintPtr04} [{nuintPtr04.GetType().FullName}]");
